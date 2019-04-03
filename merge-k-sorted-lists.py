e88798ace6bf081ae64e651409a10d8aa0e62bc3
Abirdcfly <fp544037857@gmail.com>
2019-04-03 15:34:00 +0800

    update:merge-k-sorted-lists 增加字典排序法

--- merge-k-sorted-lists.py
+++ merge-k-sorted-lists.py
@@ -30,3 +30,45 @@ class Solution(object):
                 del lists[x]
             tail.next = None
         return min.next
+
+# 字典排序法
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        z = dict()
+        for i in lists:
+            if i :
+                p = i
+                while p:
+                    n = p.next
+                    if p.val in z:
+                        t = z[p.val]
+                        while t.next:
+                            t = t.next
+                        t.next = p
+                    else:
+                        z[p.val] = p
+                    p.next = None
+                    p = n
+        zk = z.keys()
+        zk.sort()
+        if not zk:
+            return None
+        else:
+            ret = ListNode(None)
+            t = ret
+        for i in zk:
+            t.next = z[i]
+            while t.next:
+                t = t.next
+        return ret.next
+
