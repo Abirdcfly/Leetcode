651014351dc70f93d43c1e385d2c9d3cb24ae79c
Abirdcfly <fp544037857@gmail.com>
2019-04-24 19:29:25 +0800

    add 24 go

---
+++ 24-Swap-Nodes-in-Pairs.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	next := head.Next
+	head.Next, next.Next = swapPairs(head.Next.Next), head
+	return next
+}
