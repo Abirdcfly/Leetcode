3ad9cdbdb4a3a62012261c7d960bddea94ccd87b
Abirdcfly <fp544037857@gmail.com>
2019-04-24 16:47:02 +0800

    add 234 go

---
+++ 234-Palindrome-Linked-List.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	var next, pre *ListNode = nil, nil
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		next = slow.Next
+		slow.Next = pre
+		pre = slow
+		slow = next
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+	for pre != nil && pre.Val == slow.Val {
+		pre = pre.Next
+		slow = slow.Next
+	}
+	return pre == nil
+}
