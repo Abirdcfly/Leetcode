84affa0f33e72d2229f1e1a3646c53ab730369eb
Abirdcfly <fp544037857@gmail.com>
2019-05-23 07:03:32 +0800

    .

--- 50-powx-n.go
+++ 50-powx-n.go
@@ -11,3 +11,19 @@ func myPow(x float64, n int) float64 {
 	}
 	return myPow(x*x, n/2)
 }
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	pow := 1.0
+	for n != 0 {
+		if n&1 != 0 {
+			pow = pow * x
+		}
+		x = x * x
+		n >>= 1
+	}
+	return pow
+}
