fc314d111e905c9a06fdb0a1167fbe7664beb82c
Abirdcfly <fp544037857@gmail.com>
2019-05-23 09:17:15 +0800

    .

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.go
@@ -0,0 +1,16 @@
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	res := 0
+	min := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		}
+		if res < prices[i]-min {
+			res = prices[i] - min
+		}
+	}
+	return res
+}
