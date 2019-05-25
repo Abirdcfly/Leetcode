6f68d7eda6e4c66709827a11dda278afadd83398
Abirdcfly <fp544037857@gmail.com>
2019-05-25 19:02:57 +0800

    .

--- 22-Generate-Parentheses.go
+++ 22-Generate-Parentheses.go
@@ -25,3 +25,26 @@ func generateParenthesis(n int) []string {
 	}
 	return res
 }
+
+func generateParenthesis(n int) []string {
+	r := make([]string, 0)
+	help(&r, "", 0, 0, n)
+	return r
+}
+
+// https://www.youtube.com/watch?v=XF0wh8M2A6E
+func help(res *[]string, s string, l, r, n int) {
+	if len(s) == 2*n {
+		*res = append(*res, s)
+		return
+	}
+	if l < n {
+		help(res, s+"(", l+1, r, n)
+	}
+	if r < l {
+		help(res, s+")", l, r+1, n)
+	}
+}
+
+//https://zxi.mytechroad.com/blog/searching/leetcode-22-generate-parentheses/
+//核心实际都是对生成的那棵树，剪掉不合适的枝叶，后的结果。前面的代码类似BFS，上面一行链接是DFS
