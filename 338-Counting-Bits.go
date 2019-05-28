3f780934cda4f7a234a6ba5976fe4de8dd40deba
Abirdcfly <fp544037857@gmail.com>
2019-05-28 12:52:21 +0800

    .

--- 338-Counting-Bits.go
+++ 338-Counting-Bits.go
@@ -6,3 +6,14 @@ func countBits(num int) []int {
 	}
 	return res
 }
+
+import "math/bits"
+func countBits(num int) []int {
+    res := make([]int,num+1)
+    for i:=1;i<=num;i++{
+        //res[i] = res[i>>1] + i&1
+        //res[i] = res[i&(i-1)] + 1
+        res[i] = bits.OnesCount64(uint64(i))
+    }
+    return res
+}
