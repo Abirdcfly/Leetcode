ec5796ed2e209a18e4d5d0283ea24f293036193b
Abirdcfly <fp544037857@gmail.com>
2019-05-28 13:13:24 +0800

    .

--- 52-N-Queens-II.go
+++ 52-N-Queens-II.go
@@ -22,3 +22,24 @@ func dfs(r, n int, res *int, row, pie, na []bool, resCur []int) {
 		row[c], pie[c+r], na[c-r+n] = false, false, false
 	}
 }
+
+//用位运算也可以做
+func totalNQueens(n int) int {
+	res := 0
+	dfs(0, n, &res, 0, 0, 0)
+	return res
+}
+
+func dfs(r, n int, res *int, col, pie, na int) {
+	if r >= n {
+		(*res)++
+		return
+	}
+	bits := (^(col | pie | na)) & (1<<uint(n) - 1)
+	for bits != 0 {
+		p := bits & (-bits)
+		dfs(r+1, n, res, col|p, (pie|p)<<1, (na|p)>>1)
+		bits = bits & (bits - 1)
+	}
+	return
+}
