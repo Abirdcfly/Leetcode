bcbe18ee60a2644fa9d783e5adaa2921558cac09
Abirdcfly <fp544037857@gmail.com>
2019-05-31 11:46:51 +0800

    .

--- 367-Valid-Perfect-Square.go
+++ 367-Valid-Perfect-Square.go
@@ -3,6 +3,8 @@ func isPerfectSquare(num int) bool {
 	for l <= r {
 		m = l + (r-l)>>1
 		if m*m == num {
+			//一般是num/m == m 处理，为了所谓不溢出。M*M很可能太大溢出。但是这里用除法可能会遇到 5/2==2
+			//的情况，显然，5不是某个数的平方
 			return true
 		} else if m*m > num {
 			r = m - 1
