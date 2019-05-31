1a85cf7c7c5d65cb07ec32cdf6b577ba11203c3e
Abirdcfly <fp544037857@gmail.com>
2019-05-31 10:07:13 +0800

    .

--- 37-Sudoku-Solver.go
+++ 37-Sudoku-Solver.go
@@ -58,3 +58,59 @@ func dfs(r, c int, board [][]byte, row, col, box []map[byte]bool) bool {
 	}
 	return false
 }
+
+//自己理解写的
+func solveSudoku(board [][]byte) {
+	n := len(board)
+	row, col, box := [9][9]bool{}, [9][9]bool{}, [9][9]bool{}
+	check(n, board, &row, &col, &box)
+	dfs(0, 0, n, board, &row, &col, &box)
+	return
+}
+
+func dfs(r, c, n int, board [][]byte, row, col, box *[9][9]bool) bool {
+	if r >= n {
+		return true
+	}
+	nr := r
+	nc := (c + 1) % n
+	if nc == 0 {
+		nr++
+	}
+
+	if board[r][c] != '.' {
+		return dfs(nr, nc, n, board, row, col, box)
+	}
+
+	for i := 1; i <= 9; i++ {
+		if row[r][i-1] || col[c][i-1] || box[r/3*3+c/3][i-1] {
+			continue
+		}
+		board[r][c] = byte(i + int('0'))
+		row[r][i-1], col[c][i-1], box[r/3*3+c/3][i-1] = true, true, true
+		res := dfs(nr, nc, n, board, row, col, box)
+		if !res {
+			board[r][c] = '.'
+			row[r][i-1], col[c][i-1], box[r/3*3+c/3][i-1] = false, false, false
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func check(n int, board [][]byte, row, col, box *[9][9]bool) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			v := board[i][j]
+			if v == '.' {
+				continue
+			}
+			vint := v - '0'
+			row[i][vint-1] = true
+			col[j][vint-1] = true
+			box[i/3*3+j/3][vint-1] = true
+		}
+	}
+	return
+}
