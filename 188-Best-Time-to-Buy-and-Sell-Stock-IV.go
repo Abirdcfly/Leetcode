9850dd83ef85adc80139eb0852cc94a7bda85945
Abirdcfly <fp544037857@gmail.com>
2019-06-01 11:31:48 +0800

    .

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV.go
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV.go
@@ -2,6 +2,7 @@ import "math"
 
 //状态要定义好，下面的定义会导致超时，但是结果是正确的
 //不是状态问题，是数组太大，优化是搞2个数组就行，一个昨天一个今天，循环用，不必保留所有天的记录
+//上次问题找差了，是那种k远大于价格数组长度的时候，k应该变成价格数组长度的一半而不是继续用k。
 func Max(array ...int) int {
 	if len(array) == 0 {
 		return 0
@@ -99,4 +100,47 @@ func maxProfit(k int, prices []int) int {
 		res = Max(Max(mp[x][0]...), res)
 	}
 	return int(res)
+}
+
+//另一个ac版本。用float是因为出现了负无穷+一个数然后溢出为正的情况，无奈只能用-inf。实际是状态未定义好
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+func maxProfit(k int, prices []int) int {
+	if len(prices) == 0 || k <= 0 {
+		return 0
+	}
+	if k > len(prices)/2 {
+		k = len(prices) / 2
+	}
+	dp := make([][][]int, 2) //当天和前一天
+	for i := range dp {
+		dp[i] = make([][]int, k+1) //交易次数0到k
+		for j := range dp[i] {
+			dp[i][j] = make([]int, 2) //是否持有股票
+		}
+	}
+	//dp[i][j][k] 表示第i天最多j次交易时的最大收益，k==0是未持有股票 k==1是持有股票
+	dp[0][0][1] = -prices[0]
+	for i := 1; i <= k; i++ {
+		dp[0][i][1] = -prices[0]
+	}
+	for i := 1; i < len(prices); i++ {
+		ith, last := i%2, (i-1)%2
+		dp[ith][0][1] = max(-prices[i], dp[last][0][1])
+		for j := 1; j <= k; j++ {
+			dp[ith][j][0] = max(dp[last][j-1][1]+prices[i], dp[last][j][0])
+			dp[ith][j][1] = max(dp[last][j][0]-prices[i], dp[last][j][1])
+		}
+	}
+
+	ith := (len(prices) - 1) % 2
+	res := 0
+	for i := 0; i <= k; i++ {
+		res = max(res, dp[ith][i][0])
+	}
+	return res
 }
