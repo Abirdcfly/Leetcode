f8abd362f584b931382490e2c9e48fb2f0e4f399
Abirdcfly <fp544037857@gmail.com>
2019-06-01 14:52:49 +0800

    .

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.go
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.go
@@ -40,3 +40,50 @@ func maxProfit(prices []int) int {
 }
 
 //https://www.youtube.com/watch?v=oL6mRyTn56M 这个很清晰
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	dp := make([][]int, len(prices)) // dp[i][j] 代表当天状态是持有还是未持有
+	for i := range dp {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][1] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		if i < 2 { // 只有第2天是没有冷却一说的。
+			dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+		} else {
+			dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i])
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+	}
+	return dp[len(prices)-1][0]
+}
+
+//上面方法的优化
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	dp := [2][2]int{} //前一个是今天昨天，后一个是是否持有股票
+	dp[0][1] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		t, y, yby := i%2, (i+1)%2, (i+2)%2 // 今天 昨天 前天
+		dp[t][1] = max(dp[y][1], dp[yby][0]-prices[i])
+		dp[t][0] = max(dp[y][0], dp[y][1]+prices[i])
+	}
+	return dp[(len(prices)-1)%2][0]
+}
