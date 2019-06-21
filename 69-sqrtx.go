cf80922047953957ca1b7e9d2b663e295fb4de70
Abirdcfly <fp544037857@gmail.com>
2019-06-21 16:51:52 +0800

    .

--- 69-sqrtx.go
+++ 69-sqrtx.go
@@ -24,3 +24,48 @@ func mySqrt(x int) int {
 }
 
 //https://leetcode.com/problems/sqrtx/discuss/25047/A-Binary-Search-Solution/24042
+
+//使用二分 求第一个大于等于
+func mySqrt(x int) int {
+    if x == 0{
+        return 0
+    }
+    s,e,m :=1,x,0
+    for s<e{
+        m = (s+e)/2
+        if m2:=m*m; m2 == x{
+            return m
+        }else if m2< x{
+			//额外步骤，因为求的实际是最大小于
+            if (m+1)*(m+1) > x{
+                return m
+            } 
+            s = m+1
+        }else{
+            e = m
+        }
+    }
+    return s
+}
+
+//另一种用大于等于第一个数的实现。
+func mySqrt(x int) int {
+    if x == 0{
+        return 0
+    }
+    if x == 1 {
+        return 1
+    }
+    s,e,m :=1,x,0
+    for s<e{
+        m = (s+e)/2
+        if m2:=m*m; m2 == x{
+            return m
+        }else if m2 < x{
+            s = m +1
+        }else{
+            e = m 
+        }
+    }
+    return s-1 //除去1这种例外,没进循环体。其他得出的返回值或者是正好等于，或者是大于，大于的情况减1就好
+}
