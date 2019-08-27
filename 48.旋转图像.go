809e24bd84ea0ce4f7cfd33e9bb6971edb80c90b
Abirdcfly <fp544037857@gmail.com>
2019-08-27 21:55:00 +0800

    feat(st): st

---
+++ 48.旋转图像.go
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=golang
+ *
+ * [48] 旋转图像
+ *
+ * https://leetcode-cn.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (63.72%)
+ * Likes:    270
+ * Dislikes: 0
+ * Total Accepted:    32.2K
+ * Total Submissions: 50.5K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ *
+ * 说明：
+ *
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ *
+ * 示例 1:
+ *
+ * 给定 matrix =
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [4,5,6],
+ * ⁠ [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * ⁠ [7,4,1],
+ * ⁠ [8,5,2],
+ * ⁠ [9,6,3]
+ * ]
+ *
+ *
+ * 示例 2:
+ *
+ * 给定 matrix =
+ * [
+ * ⁠ [ 5, 1, 9,11],
+ * ⁠ [ 2, 4, 8,10],
+ * ⁠ [13, 3, 6, 7],
+ * ⁠ [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * ⁠ [15,13, 2, 5],
+ * ⁠ [14, 3, 4, 1],
+ * ⁠ [12, 6, 8, 9],
+ * ⁠ [16, 7,10,11]
+ * ]
+ *
+ *
+ */
+func rotate(matrix [][]int)  {
+	if len(matrix) == 0 {
+		return
+	}
+	res := make([][]int, len(matrix))
+	for i:= range res{
+		res[i] = make([]int, len(matrix))
+	}
+	for i:= range matrix{
+		for j:= range matrix[i]{
+			t := len(matrix) - 1 - i
+			res[j][t] = matrix[i][j]
+		}
+	}
+	copy(matrix, res)
+}
+
